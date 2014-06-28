df512a48278f3494116f777531af8b954af4bcbe
wgui1 <guienator@gmail.com>
2014-06-28 17:16:50 +0800

    add sprint security

---
+++ src/main/java/cn/timeoff/config/TimeoffSecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package cn.timeoff.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class TimeoffSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
