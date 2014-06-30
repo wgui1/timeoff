eae3b3cbbe694fb82ef430f1d63aacc890c7dcc4
wgui1 <guienator@gmail.com>
2014-06-30 19:17:04 +0800

    add UserDetailManager declaration

--- src/main/java/cn/timeoff/repository/UserRepository.java
+++ src/main/java/cn/timeoff/repository/UserRepository.java
@@ -4,11 +4,10 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import cn.timeoff.model.Employee;
 import cn.timeoff.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
 
-	List<Employee> findByLastName(String lastName);
+	List<User> findByUsername(String username);
 	
 }
