6f16b947aed03bbeec4e5d6fbc5c9c91c5129a96
wgui1 <guienator@gmail.com>
2014-09-28 09:06:22 +0800

    data model, in progress

--- src/main/java/cn/timeoff/model/TimeoffSetting.java
+++ src/main/java/cn/timeoff/model/TimeoffSetting.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -30,18 +31,30 @@ public class TimeoffSetting {
     private List<TimeoffPolicy> timeoffPolicies = new ArrayList<TimeoffPolicy>();
 
     @OneToOne
+    @JoinTable( name="setting_timeoffpolicy",
+                joinColumns = @JoinColumn(name = "timeoff_setting_id"),
+                inverseJoinColumns = @JoinColumn(name = "timeoff_policy_id")
+    )
     private TimeoffPolicy timeoffPolicy;
-    
-    @OneToMany(mappedBy="timeoffSetting")
+
+	@OneToMany(mappedBy="timeoffSetting")
     private List<AllowancePolicy> allowancePolicies = new ArrayList<AllowancePolicy>();
 
     @OneToOne
+    @JoinTable( name="setting_allowancepolicy",
+                joinColumns = @JoinColumn(name = "timeoff_setting_id"),
+                inverseJoinColumns = @JoinColumn(name = "allowance_policy_id")
+    )
     private AllowancePolicy allowancePolicy;
 
     @OneToMany(mappedBy="timeoffSetting")
     private List<PartialYearRate> partialYearRates = new ArrayList<PartialYearRate>();
 
     @OneToOne
+    @JoinTable( name="setting_partialyearrate",
+                joinColumns = @JoinColumn(name = "timeoff_setting_id"),
+                inverseJoinColumns = @JoinColumn(name = "partial_year_rate_id")
+    )
     private PartialYearRate partialYearRate;
     
     @LastModifiedBy
@@ -153,5 +166,67 @@ public class TimeoffSetting {
     public void setLastModifiedTime(Timestamp lastModifiedTime) {
         this.lastModifiedTime = lastModifiedTime;
     }
+    
+    public boolean isActive() {
+    	if (timeoffPolicy != null) {
+    		isActive = timeoffPolicy.isActive();
+    		if (isActive != null) {
+                return isActive;
+    		}
+    	}
+        if (organization.getUpperLevel() != null)
+    	}
+	}
 
+	public int getRenewal() {
+		return timeoffPolicy.getRenewal();
+	}
+
+	public int getCarryOver() {
+		return timeoffPolicy.getCarryOver();
+	}
+
+	public int getAccuralInterval() {
+		return timeoffPolicy.getAccuralInterval();
+	}
+
+	public int getAccuralBy() {
+		return timeoffPolicy.getAccuralBy();
+	}
+
+	public int getAccuralLimit() {
+		return timeoffPolicy.getAccuralLimit();
+	}
+
+	public boolean isBalanceRequired() {
+		return timeoffPolicy.isBalanceRequired();
+	}
+
+	public boolean isAutoApproval() {
+		return timeoffPolicy.isAutoApproval();
+	}
+
+	public boolean isRequestEanbled() {
+		return timeoffPolicy.isRequestEanbled();
+	}
+
+	public int getAccrual() {
+		return allowancePolicy.getAccrual();
+	}
+
+	public int getMax() {
+		return allowancePolicy.getMax();
+	}
+
+	public int getAccrualCycle() {
+		return allowancePolicy.getAccrualCycle();
+	}
+
+	public boolean isPropotional() {
+		return partialYearRate.isPropotional();
+	}
+
+	public List<Float> getRatePerMonth() {
+		return partialYearRate.getRatePerMonth();
+	}
 }
