d082395f6efdf6f948172166bb0aed2f731155c4
wgui1 <guienator@gmail.com>
2014-11-05 22:51:00 +0800

    add resource handler for assets, to get css and js resource

--- src/main/java/cn/timeoff/Application.java
+++ src/main/java/cn/timeoff/Application.java
@@ -3,13 +3,26 @@ package cn.timeoff;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @EnableAutoConfiguration
 @EnableWebMvc
 public class Application {
+
+	@Bean
+	public WebMvcConfigurerAdapter webConfig() {
+		return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+            }
+		};
+	}
 
     public static void main(String[] args) {
         @SuppressWarnings("unused")
