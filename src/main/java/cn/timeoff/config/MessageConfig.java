c1cc2181f652a3dfd7e30656a39406ad03361bb2
wgui1 <guienator@gmail.com>
2014-11-24 01:25:09 +0800

    use reloadable message source

--- src/main/java/cn/timeoff/config/MessageConfig.java
+++ src/main/java/cn/timeoff/config/MessageConfig.java
@@ -3,15 +3,15 @@ package cn.timeoff.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @Configuration
 public class MessageConfig {
 
 	@Bean
 	public MessageSource messageSource() {
-		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
-		ms.setBasename("cn.timeoff.messages");
+		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+		ms.setBasename("classpath:messages");
 		return ms;
 	}
 
